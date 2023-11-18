0bbfb6cfce916b35b7aed81fefe06aea39c5488f
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-11-18 07:13:53 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import VideoPlayer from "./components/Video/VideoPlayer.vue";
+import VideoDetails from "./components/Video/VideoDetails.vue";
+import Chat from "./components/chat/Chat.vue";
+
+import {ref} from 'vue';
+let newDescription = ref("");
+
+const updateVideo = (description) => {
+  console.log(description);
+  newDescription.value = description;
+};
+
+
+
+</script>
+
+<template>
+  <div class="tiktok">
+    <div>
+      <VideoPlayer @update:video-Description="updateVideo" />
+    </div>
+    <div>
+      <VideoDetails :description="newDescription" />
+      <Chat />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .tiktok{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
---
+++ src/components/chat/Chat.vue
@@ -0,0 +1,30 @@
+<script setup>
+    //import ref
+    import { ref, reactive, onMounted } from 'vue'
+
+    let message = ref("Allooks"); //int, string, boolean
+    let allMessages = reactive({
+        data: ["heel", "even", "simpel", "doen"],
+    }); //array, object
+
+    function sendMessage() {
+        allMessages.data.push(message.value);
+        message.value = "";
+    }
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="m in allMessages.data">{{ m }}</li>
+    </ul>
+
+    <div>
+        <input v-model="message" type="text" placeholder="Type your message here">
+        <button @click="sendMessage">Send</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
